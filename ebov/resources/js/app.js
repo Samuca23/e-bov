719835ea51bd2943bb14d8c82160cbcd47d1fe08
samuca23 <samuelchiodini@hotmail.com>
2023-03-12 23:10:11 -0300

    Teste do Vue com Laravel

--- ebov/resources/js/app.js
+++ ebov/resources/js/app.js
@@ -1,9 +1,11 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import App from './components/App.vue'
+import Teste from './components/Teste.vue'
 
 const app = createApp();
 
 app.component('app', App);
+app.component('teste', Teste);
 
 app.mount('#app');
---
+++ ebov/resources/js/components/Teste.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Teste Vue</h2>
+</template>
